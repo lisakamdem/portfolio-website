8d0b2fc2e054d7879cb05d9adc22cf1aa77be8a6
lisakamdem <lisanandy.kamdempokam@edu.sait.ca>
2024-06-06 17:14:38 -0600

    adjusting imgaes

--- app/Mayhem/page.js
+++ app/Mayhem/page.js
@@ -8,7 +8,7 @@ export default function Mayhem() {
         <div className="w-full">
             <div className="w-screen h-[50vh] relative">
                 <div className="absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10">
-                    <Image src="/./assets/Mayhem.jpg" alt="/" className="absolute z-1" layout="fill" objectFit="cover" />
+                    <Image src="/./assets/Mayhem.jpg" alt="mayhem logo" className="absolute z-1" layout="fill" objectFit="cover" />
                         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] translate-x-[-50%] translate-y-[-50%] z-10 p-2">
                             <h2 className="py-2 text-white">Mayhem</h2>
                             <h3 className="text-white">React Native</h3>
